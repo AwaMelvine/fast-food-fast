64c588fa8c08d1860404b1e365170f60383cd630
Awa Melvine <melvineawa@gmail.com>
2018-08-28 20:15:22 -0700

    Finished creating navbar. Started work on Landing page

---
+++ ui/javascripts/scripts.js
@@ -0,0 +1,16 @@
+var menuIcon = document.querySelector('.menu-icon');
+var navLinks = document.querySelector('.menu-links');
+var nav = document.getElementsByTagName('nav')[0];
+
+menuIcon.addEventListener('click', function (e) {
+  console.log(menuIcon.classList);
+  navLinks.classList.toggle('showing');
+});
+
+window.addEventListener('scroll', function () {
+  if (document.documentElement.scrollTop >= 30) {
+    nav.classList.add('solid');
+  } else {
+    nav.classList.remove('solid');
+  }
+});
