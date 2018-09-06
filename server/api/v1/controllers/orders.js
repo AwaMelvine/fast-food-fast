0ccc16b7be614dcf1ea60ef2c5b28c4079d340ef
Awa Melvine <melvineawa@gmail.com>
2018-09-06 07:09:25 -0700

    Create data structure for orders

--- server/api/v1/controllers/orders.js
+++ server/api/v1/controllers/orders.js
@@ -1,4 +1,5 @@
-const allOrders = [];
+// Import data structure for orders
+import { allOrders, Order } from '../models/Order';
 
 export const getAllOrders = (req, res) => {
   res.status(200).json(allOrders);
---
+++ server/api/v1/models/Order.js
@@ -0,0 +1,16 @@
+import toInt from '../helpers/functions';
+
+export const allOrders = [];
+
+export class Order {
+  constructor(order) {
+    this.id = order.id ? toInt(order.id) : 0;
+    this.customerId = order.customerId ? order.customerId.toString() : null;
+    this.itemId = order.itemId ? toInt(order.itemId) : 0;
+    this.quantity = order.quantity ? toInt(order.quantity) : 0;
+    this.totalPrice = order.totalPrice ? toInt(order.totalPrice) : 0;
+    this.orderDate = order.orderDate || null;
+    this.dateToDeliver = order.dateToDeliver || null;
+    this.orderStatus = order.orderStatus ? order.orderStatus.toString() : null;
+  }
+}
