71de08b000bcff2200b84610277a938a57c740a5
Awa Melvine <melvineawa@gmail.com>
2018-09-07 16:46:43 -0700

    Create a welcome / endpoint

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ app.use(bodyParser.json());
 app.use('/api/v1/orders', orders);
 app.use('/api/v1/foodItems', foodItems);
 
+app.use('/', (req, res) => {
+  res.status(200).json({ message: 'Check out Fast-Food-Fast api at /api/v1/orders or /api/v1/foodItems' });
+});
+
+app.use('/api/v1/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to version 1 of Fast Food Fast API.\n\n Go to /api/v1/orders or /api/v1/foodItems to use our api' });
+});
+
 app.use('/*', (req, res) => {
-  res.status(404).json({ message: 'Page Not Found. Please go to /api/vi/orders to use our api' });
+  res.status(404).json({ message: 'Page Not Found. Please go to /api/v1/orders to use our api' });
 });
 
 if (process.env.NODE_ENV !== 'test') {
