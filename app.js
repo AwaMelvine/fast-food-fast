fe15f25aa341bf20b828f5688ea49c542068942c
Awa Melvine <melvineawa@gmail.com>
2018-09-07 16:41:26 -0700

    Create a middleware to handle case where Page Not Found

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json());
 app.use('/api/v1/orders', orders);
 app.use('/api/v1/foodItems', foodItems);
 
+app.use('/*', (req, res) => {
+  res.status(404).json({ message: 'Page Not Found. Please go to /api/vi/orders to use our api' });
+});
+
 if (process.env.NODE_ENV !== 'test') {
   app.listen(process.env.PORT || 5000, () => console.log('Server started at localhost:5000'));
 }
