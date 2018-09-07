e3bc377bccf5df9fe06f58e3c2aecc1b584486e0
Awa Melvine <melvineawa@gmail.com>
2018-09-07 08:40:25 -0700

    Implement validation of food item

--- api/v1/routes/foodItems.js
+++ api/v1/routes/foodItems.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as foodItemsController from '../controllers/foodItems';
+import validateFoodItem from '../helpers/validateFoodItem';
 
 const router = express.Router();
 
 
 router.get('/', foodItemsController.getAllFoodItems);
 router.get('/:foodItemId', foodItemsController.getFoodItemById);
-router.post('/', foodItemsController.createFoodItem);
-router.put('/:foodItemId', foodItemsController.updateFoodItem);
+router.post('/', validateFoodItem, foodItemsController.createFoodItem);
+router.put('/:foodItemId', validateFoodItem, foodItemsController.updateFoodItem);
 router.delete('/:foodItemId', foodItemsController.deleteFoodItem);
 
 
