94f3e4ca6a85a966693ec63c04ed15a671045c0c
Awa Melvine <melvineawa@gmail.com>
2018-09-21 23:39:01 -0700

    Implement validation of categories

--- api/v1/routes/categories.js
+++ api/v1/routes/categories.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import categoriesController from '../controllers/categories';
+import validateCategory from '../helpers/validateCategories';
 
 const router = express.Router();
 
 
 router.get('/', categoriesController.getAllCategories);
 router.get('/:categoryId', categoriesController.getCategoryById);
-router.post('/', categoriesController.createCategory);
-router.put('/:categoryId', categoriesController.updateCategory);
+router.post('/', validateCategory, categoriesController.createCategory);
+router.put('/:categoryId', validateCategory, categoriesController.updateCategory);
 router.delete('/:categoryId', categoriesController.deleteCategory);
 
 
