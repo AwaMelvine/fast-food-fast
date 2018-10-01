eef888d067f4deaf7cf3d48b9aedf56840aeb5be
Awa Melvine <melvineawa@gmail.com>
2018-10-01 13:50:07 -0700

    chore(travisDB_URL) Configure database with travisCI
    In travis.yml file, add a script in the before_script section
    to create the test database.
    
    [Fixes#160878156]

--- api/v1/routes/orders.js
+++ api/v1/routes/orders.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import ordersController from '../controllers/orders';
 import validateOrder from '../helpers/validateOrder';
+import authenticate from '../helpers/authenticate';
+
 
 const router = express.Router();
 
