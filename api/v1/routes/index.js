a2807ce933af3fe2d2057f7b282a9fae0efc0af7
Awa Melvine <melvineawa@gmail.com>
2018-10-16 15:31:26 -0700

    feat(menu) Implement create menu item
    On the admin interface on the client, implement frontend food
    item creation for an admin user.
    
    [menu#161257071]

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -7,6 +7,7 @@ import foodItems from './foodItems';
 
 const router = new Router();
 
+
 router.use('/api/v1/auth', auth);
 router.use('/api/v1/orders', orders);
 router.use('/api/v1/users', users);
@@ -14,8 +15,8 @@ router.use('/api/v1/categories', categories);
 router.use('/api/v1/menu', foodItems);
 
 
-// router.use('/*', (req, res) => {
-//   res.status(404).json({ message: 'Page Not Found. Please go to /api/v1/orders to use our api' });
-// });
+router.use('/*', (req, res) => {
+  res.status(404).json({ message: 'Page Not Found. Please go to /api/v1/orders to use our api' });
+});
 
 export default router;
--- client/admin/foodItems/menu.js
+++ client/admin/foodItems/menu.js
@@ -1,4 +1,9 @@
-const itemsContainer = document.getElementById('items-container');
+const itemsTableBody = document.getElementById('items-table-body');
+const messageDiv = document.getElementById('message-div');
+
+function displayMessage() {
+
+}
 
 function fetchItems() {
   fetch('https://fast-food-fast-service.herokuapp.com/api/v1/menu', {
@@ -9,10 +14,61 @@ function fetchItems() {
     }),
   }).then(res => res.json())
     .then((response) => {
-      // TODO: list menu items on page
-      console.log(response.data);
+      let itemsRows = '';
+
+      if (response.data.length === 0) {
+        itemsTableBody.innerHTML = `<tr>
+            <td colspan="7" class="center-text">No items yet!</td>
+          </tr>`;
+        return;
+      }
+
+      response.data.forEach((item, index) => {
+        const tempIndex = index + 1;
+        itemsRows = `${itemsRows}<tr>
+            <td>${tempIndex}</td>
+            <td>${item.name}</td>
+            <td>
+              <img src="${item.image}" alt="">
+            </td>
+            <td>${item.description.substring(0, 60)}</td>
+            <td>${item.unit_price}</td>
+            <td>${item.quantity}</td>
+            <td>
+              <a href="edit_food_item.html" class="btn btn-green btn-sm">
+                <i class="fa fa-pencil"></i> Edit</a>
+            </td>
+            <td>
+              <button type="button" id="delete-item-btn" onClick="deleteItem(${item.id})" data-id="${item.id}" class="btn btn-red btn-sm">
+                <i class="fa fa-trash"></i> Delete</button>
+            </td>
+          </tr>`;
+      });
+      itemsTableBody.innerHTML = itemsRows;
     })
     .catch(error => console.error('Error:', error));
 }
 
+
+function deleteItem(id) {
+  const userInfo = localStorage.getItem('userInfo');
+  const token = userInfo.token;
+
+  fetch(`https://fast-food-fast-service.herokuapp.com/api/v1/menu/${id}`, {
+    method: 'DELETE',
+    mode: 'cors',
+    headers: new Headers({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      Authorization: `token ${token}`,
+    }),
+  })
+    .then(res => res.json())
+    .then(() => {
+      fetchItems();
+    });
+}
+
 fetchItems();
+
+console.log(document.getElementById('delete-item-btn'));
