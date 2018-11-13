5e6cf913b9ee4fca717a2f782de20d18458e9cd9
Awa Melvine <melvineawa@gmail.com>
2018-11-13 05:17:45 -0800

    chore(navbarLinks) Restrict admin pages access
    - Ensure only admin users can view the admin pages.
    - Display logged in username on the admin navbar
    - Implement logout for admin user. Redirect user to index page after
    logout
    
    [admin#161859321]

---
+++ client/admin/admin.js
@@ -0,0 +1,27 @@
+const userInfoLink = document.getElementById('user-info-link');
+
+const cart = JSON.parse(localStorage.getItem('cart'));
+const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+// display user info
+userInfoLink.innerHTML = `<span>
+    <i class="fa fa-user"></i>
+    ${userInfo.username}
+    <br>
+    <button onclick="logout()" id="logout-link">logout</button>
+    </span>`;
+
+function logout() {
+  localStorage.removeItem('userInfo');
+  window.location.href = '../index.html';
+}
+
+
+function adminOnly() {
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  if (userInfo.role !== 'admin') {
+    window.location.href = '../index.html';
+  }
+}
+
+adminOnly();
